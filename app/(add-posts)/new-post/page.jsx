b6387a586aebdc2256b7227139d835b668be6bab
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-10-18 20:28:30 +0300

    new-add cssi eklendi

--- app/(add-posts)/new-post/page.jsx
+++ app/(add-posts)/new-post/page.jsx
@@ -3,7 +3,7 @@ import {SavedPost} from "./actions";
 export default function NewPost() {
   return (
     <div>
-      <form action={SavedPost}>
+      <form className="submitPost" action={SavedPost}>
         <input type="text" name="title" placeholder="yazı başlığını giriniz" />
         <br />
         <textarea name="content" id="" placeholder="yazı içeriğini giriniz "></textarea>
